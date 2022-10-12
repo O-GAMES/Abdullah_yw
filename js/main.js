7a7ad767244b12c601b15647af424926f5eeda05
O-GAMES <79658234+O-GAMES@users.noreply.github.com>
2022-10-12 06:54:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,69 @@ icon.onclick = function () {
     list.classList.toggle("active")
 }
 
+// with fetch
+
+
+
+ 
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://linktr.ee/Abdullah_yw')}`)
+.then(
+    function(response) {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
+
+    }).then(function (data) {
+
+        
+        let linkTer  = data.contents;
+        
+for (let i = 0; i < linkTer.length; i++) {
+
+    let myLinks = document.createElement('a');
+    let myLinksTitle = linkTer.slice(i, i + 250).split("<")[0].slice(3);
+    let myLinksText = document.createTextNode(myLinksTitle);
+
+    if (linkTer.slice(i, i+ 3) === 'I">') {
+        
+        myLinks.className = "links";
+
+        myLinks.appendChild(myLinksText)
+
+        document.getElementById('links-cards').appendChild(myLinks)
+
+        myLinks.setAttribute("href", getLinks)
+        myLinks.setAttribute("target", "_blank")
+
+        myLinks.style.cssText = `
+        padding: 1.5rem;
+        box-shadow: rgb(0 0 0 / 3%) 0px 0px 20px 9px;
+        display: flex;
+        border-radius: 1rem;
+        color: var(--second-color);
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        cursor: pointer;
+        `;
+
+        
+
+    }
+
+    if (linkTer.slice(i, i+ 14) === '<a href="http:') {
+
+        var getLinks = linkTer.slice(i, i + 200).split(`"`)[1].split(' ')[0]
+
+    }
+    
+}
+
+    })  
+
+
+
+
+
 
 let swiperTestimonial = new Swiper(".swiper", {
     spaceBetween: 20,
@@ -16,20 +79,20 @@ let swiperTestimonial = new Swiper(".swiper", {
     loop: true, 
     grabCursor: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
     },
     breakpoints: {
         576: {
-          slidesPerView: 3,
+        slidesPerView: 3,
         },
         768: {
-          slidesPerView: 3,
-          spaceBetween: 40,
+        slidesPerView: 3,
+        spaceBetween: 40,
         },
-      },
-  });
+        },
+    });
 
 
 const sr = ScrollReveal({
@@ -48,7 +111,7 @@ sr.reveal(`#hero-img, .title, .story-text, .contact_card, .copyright, .footer-so
 
 sr.reveal(`#hero-text-p, #hero-text-h4, #hero-text-h1, #about-img, .text-title, .contact_form, .contact_title`, { origin: "rigth", interval: 100 });
 
-    sr.reveal(`.socil-icon, .hero, .project_card, .btn, .swiper, .projects_filters, .contact_content, .sponsor, .sponsor-img`, { origin: "bottom", interval: 100 });
+    sr.reveal(`.socil-icon, .hero, .project_card, .btn, .swiper, .projects_filters, .contact_content, .sponsor, .sponsor-img, .links`, { origin: "bottom", interval: 100 });
 
 
 
@@ -61,14 +124,14 @@ let mixerPortfolio = mixitup('.project_container', {
     }
 });
 
-/* Link active work */ 
-const linkWork = document.querySelectorAll('.projects_item')
+// /* Link active work */ 
+// const linkWork = document.querySelectorAll('.projects_item')
 
-function activeWork(){
-    linkWork.forEach(l=> l.classList.remove('activ_item '))
-    this.classList.add('activ_item')
-}
+// function activeWork(){
+//     linkWork.forEach(l=> l.classList.remove('activ_item '))
+//     this.classList.add('activ_item')
+// }
 
-linkWork.forEach(l=> l.addEventListener('click', activeWork()))
+// linkWork.forEach(l=> l.addEventListener('click', activeWork()))
 
 

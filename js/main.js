05af49c467109d50ac0d5ab15d857be3c3100745
O-GAMES <79658234+O-GAMES@users.noreply.github.com>
2022-11-11 14:34:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,236 @@
+let lang = {
+    ar: {
+        headerH2: "عبدالله العلاوي",
+        contact: "📮 تواصل",
+        links: "🔗 روابط",
+        shot: "📸 لقطة",
+        myWorks: "📍 أعمالي",
+        myStory: "📝 قصتي",
+        heroWelcome: "أهلا معك",
+        heroAbdullah: "عبدالله العلاوي",
+        heroText: "طبيب أسنان ومؤسس مجتمع مسافة، أعمل على تحسين جودة حياتي اليومية<br>وأشارك معك توثيق الرحلة بمواقع التواصل الاجتماعي.",
+        forMore: "للمزيد",
+        aboutTitle: `<span class="title-dot"></span> قصتي`,
+        storyText: `
+        حياااك الله من جديد <br>
+        معك عبدالله العلاوي طبيب أسنان سعودي <br>
+       وفي الوقت الحالي أعمل يوميا على تحسين حياتي 🚀<br>
+       صحيح أنّ رحلة التحسين شاقة وغير منتهية ولكنّها أيضاً ممتعة، وبوجودك معي فإن التأثير يتعداني ويصل للجميع…وهذا هو الهدف الحقيقي من كل شي!<br>
+       <br>
+       أهم محطات الرحلة كانت:<br>
+       * مجتمع مسافة الذي يهدف إلى تقريب المسافة بين شخصيتك المستقبلية والحالية من خلال معسكرات وورش عمل أو تحديات نطلقها باستمرار <br>
+       * على الصعيد الأكاديمي أسست مبادرة "أنت أنا" التي ساعدت آلاف الطلاب بحمدالله وفضله، ثم بتعاون طلاب الجامعات بالسعودية وخارجها مع طلبة الثانوية بكل حب للعطاء 🎓<br>
+       * بفضل الله تخرجت من برنامج قمم الذي يهدف لتمكين القيادات الواعدة من طلاب وطالبات جامعات المملكة العربية السعودية.<br>
+       <br>
+       حصلت بتوفيق الله على عدة جوائز .. أهمها : <br>
+       جائزة طبيب الامتياز الأفضل في التواصل – كلية طب الأسنان، جامعة الملك عبدالعزيز ، 2019<br>
+       المركز الأول بمسابقة أفضل فكرة عمل اجتماعي -جائزة الأميرة صيتة باليوم الوطني 88 ،2018<br>
+       المركز الثالث بمسابقة مدينة الملك عبدالعزيز للعلوم والتقنية- لتقديم محتوى إعلامي توعوي مسموع، 2018<br>
+       المركز الأول بجائزة وعي - مسار السناب شات، وزارة الصحة السعودية، 2018<br>
+       جائزة أم الجود للتوعية بالثقافة العامة – مؤسسة جائزة الأميرة صيتة بنت عبدالعزيز للتميز في العمل الاجتماعي، 2022.<br>
+       <br>
+       <br>
+       * وتشرفت بتمثيل المملكة العربية السعودية في عدة محافل:<br>
+       - 2019 منتدى شباب العالم بمصر<br>
+       - 2018 برنامج القيادات الإعلامية العربية الشابة بالإمارات<br>
+       - 2018 سفير برنامج MBC الأمل بالجامعات السعودية<br>
+       - 2019 مثّلت وبكل فخر خريجي جامعة الملك عبدالعزيز <a href="https://youtu.be/iv5aLFR566s" target="_blank">بكلمة الخريجين</a> بالحفل السنوي <br>
+       <br>
+       * قدمت محاضرات وخطابات تعليمية لكن يبقى أقربها لقلبي حتى اليوم: <br>
+       <a href="https://youtu.be/foBHMKhSHu4 " target="_blank">TedxKau</a> بجامعة الملك عبدالعزيز خطاب: اسمع العالم صوتك  🌍<br>
+       <br>
+       *شاركت بتقديم ورش عمل:<br>
+       - مسك المهارات<br>
+       -  TEDx جامعة طيبة واليوم الوطني فيها.br>
+       - مؤتمر اليوم العالمي للتطوع بمنطقة المدينة المنورة <br>
+       -2019,2022 سلسلة ورشة عمل (صناعة التوعية الصحية) حول مدن السعودية <br>
+       <br>
+       <br>
+       روتين التسعين هو المشروع الأهم بختام 2022م وبداية 2023 م بإذن الله <br>
+       <br>
+        والرحلة مستمرة بإذن الله…<br>`,
+        projectsTitle: `<span class="title-dot"></span> أعمالي`,
+        all: "الكل",
+        vdieos: "فيديوهات",
+        courses: "دورات",
+        blog: "المدونة",
+        projectCardCourse: "دورة رحلة الإنتاجية",
+        articlOne: "عشر أمور تركتها من حياتي",
+        articlTwo: "عادة الاستيقاظ المبّكر؟",
+        articlThree: "التدوين اليومي .. علاج فعّال", 
+        worldTitle: `<span class="title-dot"></span> لقطة`,
+        shotText: "تختصر المعنى",
+        linksTitle: `<span class="title-dot"></span> روابط`,
+        linksText: ":) اضغط .. توصل",
+        workTitle: `<span class="title-dot"></span> تواصل`,
+        workTextTitle: "للتعاون والأعمال أو التواصل",
+        cardTitel: "بريدي الشخصي",
+        cardSend: "أرسل",
+        cardTitelWh: "واتس آب",
+        cardTitelWhSend: "تواصل مباشرة",
+        contactTitle: "النشرة البريدية",
+        contactEmailText: "افكار و تجارب اشاركها على ايميلك (بدون ازعاج)",
+        writeEmail: "اكتب بريدك",
+        contactBtn: "تأكيد",
+        sponsorTitle: "شركاء منحوني ثقتهم",
+        footerContactText: "تابعني على وسأل التواصل",
+        copyrightText:"جميع الحقوق محفوظة لـ د. عبدالله العلاوي"
+        
+    },
+
+    en: {
+        headerH2: "Abdullah Alawi",
+        contact: "📮 Contact",
+        links: "🔗 Links",
+        shot: "📸 Shot",
+        myWorks: "📍 My works",
+        myStory: "📝 My story",
+        heroWelcome: "Welcome! It’s",
+        heroAbdullah: "Abdullah Al-Alawi",
+        heroText: "A dentist and the founder of Masafah’s Community. I work to enhance<br>the quality of my daily life and share it with you via social media platforms.",
+        forMore: "For more",
+        aboutTitle: `<span class="title-dot"></span> My Story`,
+        storyText: `
+        Welcome again!<br>
+        It’s Abdullah Al-Alawi, a Saudi dentist.<br>
+        Currently, I am working on improving my life 🚀<br>
+        It’s true that the journey of self-improvement is challenging and never-ending but it’s also enjoyable! And with you by my side, the impact extends far beyond myself, which is the actual goal of it all!<br>
+        <br>
+        * The significant stations of my life are:<br>
+        * Masafah Community, which seeks to bridge the gap between your future and present selves by continuously offering online camps, workshops, and challenges.<br>
+        * 
+        On the academic level, I founded the “Anta Ana” initiative, which, by the grace of God, and in cooperation with university students from Saudi Arabia and abroad, out of a love of giving, has helped thousands of high school students.<br>
+        * With God's help, I graduated from the Qimam Program, which aims to empower future leaders among male and female Saudi Arabian university students.<br>
+        <br>
+        By God's grace, I have received a number of awards, the most significant of which are:<br>
+        Doctor of Excellence Award for Best Communication - College of Dentistry, King Abdulaziz University, 2019.<br>
+        First place in the competition for the best social work idea - Princess Seetah Prize on the 88th National Day, 2018.
+        excellence in social work<br>
+
+        Third Place in competition for providing audible awareness media content, King Abdulaziz City for Science and Technology, 2018.<br>
+        First place in the Wa’ai Award – on SnapChat, Saudi Ministry of Health, 2018.<br>
+        Umm Al-Joud Award for creating general awareness of social responsibility needs in society - Foundation of the Princess Seetah bint Abdulaziz Award for Excellence in Social Work, 2022.<br>
+        <br>
+        <br>
+        I had the honor to represent the Kingdom of Saudi Arabia in several forums:<br>
+        - 2019 World Youth Forum in Egypt,.<br>
+        - 2018 Young Arab Media Leaders Program in the Emirates,.<br>
+        - 2018 Ambassador of the MBC Al-Amal Program in Saudi universities<br>
+        - 2019 Proudly represented the graduates of King Abdulaziz University with the alumni <a href="https://youtu.be/iv5aLFR566s" target="_blank">speech</a> at the annual ceremony.<br>
+        <br>
+        * I've given lectures and educational speeches, but the one that remains dearest to my heart is "Let the World Hear Your Voice 🌍" at <a href="https://youtu.be/foBHMKhSHu4 " target="_blank">TEDxKAU</a>, King Abdulaziz University.<br>
+        <br>
+        * Participated in providing workshops:<br>
+        - Misk Skills,<br>
+        - Master of Ceremonies at TEDxTaibahU, on the National Day at Taibah University.<br>
+        - The International Volunteer Day Conference in Al-Madinah Al-Munawwarah.<br>
+        <br>
+        <br>
+        God willing, "The Routine of the Ninety Days" will be Masafah's most important project (from the end of 2022 until the beginning of 2023).<br>
+        <br>
+        And the journey continues, God willing...
+        `,
+        projectsTitle: `<span class="title-dot"></span> My Works`,
+        all: "All",
+        vdieos: "Vdieos",
+        courses: "Courses",
+        blog: "blog",
+        projectCardCourse: "Productivity Journey Course",
+        articlOne: "10 Things I Have Given up on in<br> My Life",
+        articlTwo: "The Habit of Waking Up Early",
+        articlThree: "Daily Blogging", 
+        worldTitle: `<span class="title-dot"></span> Shot`,
+        shotText: "",
+        linksTitle: `<span class="title-dot"></span> Links`,
+        linksText: "Click on the link",
+        workTitle: `<span class="title-dot"></span> Contact`,
+        workTextTitle: "For cooperation, business or communication",
+        cardTitel: "Personal Email",
+        cardSend: "Send",
+        cardTitelWh: "WhatsApp",
+        cardTitelWhSend: "For direct contact",
+        contactTitle: "Newsletter",
+        contactEmailText: "To share thoughts and experiences with you<br>via email (without disturbance)",
+        writeEmail: "Enter your Email",
+        contactBtn: "Confirm",
+        sponsorTitle: "Partners who have placed their confidence in me:",
+        footerContactText: "Follow me on social media platforms",
+        copyrightText:"All rights reserved to Dr. Abdullah Al-Alawi"
+    }
+}
+
+let selectorLang = document.getElementById('lang-sele')
+selectorLang.onchange = function () {
+    let language = selectorLang.options[selectorLang.selectedIndex].value;
+    let nodes = document.querySelectorAll('[data-lang]');
+
+    for (let i = 0; i < nodes.length; i++) {
+        let key = nodes[i].getAttribute('data-lang');
+        nodes[i].innerHTML = lang[language][key];
+        
+        
+    }
+    
+    document.querySelector('.contact_form-input').placeholder = lang.en.writeEmail;
+    document.querySelector('.button').value = lang.en.contactBtn;
+
+    
+    if (selectorLang.options[selectorLang.selectedIndex].value === "en") {
+        document.getElementById('main-container').style.cssText = `
+        flex-direction: row-reverse`;
+
+        document.querySelector('.hero').style.cssText = `direction: ltr`;
+
+        document.getElementById('about-container').style.cssText = `
+        flex-direction: row-reverse`
+        
+        document.querySelector('.text-box').style.cssText = `
+        direction: ltr; 
+        margin-left: 3rem;
+        margin-right: 0;
+        `;
+        
+        
+        for (let i = 0; i < document.querySelectorAll('.title-dot').length; i++) {
+            
+            document.querySelectorAll('.title-dot')[i].style.cssText = `
+            right: auto;
+            left: -7px;
+            `;
+            
+        };
+
+
+        document.getElementById('cardH').style.cssText = `direction: ltr`;
+
+
+    } else {
+
+        document.querySelector('.contact_form-input').placeholder = lang.ar.writeEmail;
+        document.querySelector('.button').value = lang.ar.contactBtn;
+
+
+        document.getElementById('main-container').style.cssText = `
+        flex-direction: auto`;
+
+        document.querySelector('.hero').style.cssText = `direction: rtl`;
+
+        document.getElementById('about-container').style.cssText = `
+        flex-direction: row`;
+
+        document.querySelector('.text-box').style.cssText = `
+        direction: rtl; 
+        margin-left: 0;
+        margin-right: 3rem;
+        `;
+
+    }
+
+
+}
+
+
+/* ==============menu============ */
 
 let icon = document.querySelector(".icon");
 let list = document.getElementById("phone-list")
@@ -6,11 +239,66 @@ icon.onclick = function () {
     list.classList.toggle("active")
 }
 
-// with fetch
+
+/* ===========Slide img=========== */
+let swiperWrapper = document.querySelector('.swiper-wrapper');
+
+for (let i = 1; i <= 60; i++) {
+    
+    let swiperSlide = document.createElement('div');
+    let imgSlide = document.createElement('img');
+    
+    swiperSlide.className = 'swiper-slide';
+    
+    imgSlide.src = `images/word/${i}.jpg`;
+    
+    swiperSlide.appendChild(imgSlide);
+    swiperWrapper.appendChild(swiperSlide);
+    
+    
+}
+
+/* ===========sponsor img=========== */
+
+for (let i = 1; i <= 20; i++) {
+    
+    let sponsorContent = document.querySelector('.sponsor_content');
+    let imgSponsor = document.createElement('img');
+    
+    imgSponsor.className = 'sponsor-img';
+    imgSponsor.src = `images/sponsor/${i}.png`;
+    
+    sponsorContent.appendChild(imgSponsor);
+    
+}
 
 
+/* ============youtube videos========== */
+const loadVideo = (iframe) => {
+    const cid = "UCUdtq-Fvlw7_NjzduNTZsbg";
+    const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`)
+    const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
 
- 
+    fetch(reqURL)
+        .then(response => response.json())
+        .then(result => {
+        console.log(result)
+            const videoNumber = iframe.getAttribute('vnum')
+            const link = result.items[videoNumber].link;
+            const id = link.substr(link.indexOf("=") + 1);
+            iframe.setAttribute("src", `https://youtube.com/embed/${id}?controls=0&autoplay=1`);
+        })
+        .catch(error => console.log('error', error));
+}
+
+const iframes = document.getElementsByClassName('latestVideoEmbed');
+for (let i = 0, len = iframes.length; i < len; i++) {
+    loadVideo(iframes[i]);
+}
+
+
+/* ===========get links=========== */
+
 fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://linktr.ee/Abdullah_yw')}`)
 .then(
     function(response) {
@@ -21,7 +309,9 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://linktr.e
 
         
         let linkTer  = data.contents;
-        console.log(linkTer)
+        
+        
+        
 for (let i = 0; i < linkTer.length; i++) {
 
     let myLinks = document.createElement('a');
@@ -51,24 +341,22 @@ for (let i = 0; i < linkTer.length; i++) {
         cursor: pointer;
         `;
 
-        
-
+    } else {
+        // console.log("not fine")
     }
 
-    if (linkTer.slice(i, i+ 14) === '<a href="http:') {
+    
+    if (linkTer.slice(i, i+ 12) === '<a href="htt') {
 
         var getLinks = linkTer.slice(i, i + 200).split(`"`)[1].split(' ')[0]
 
     }
     
 }
-
-    })  
-
+});
 
 
-
-
+/* ===========swiper=========== */
 
 let swiperTestimonial = new Swiper(".swiper", {
     spaceBetween: 20,
@@ -94,6 +382,7 @@ let swiperTestimonial = new Swiper(".swiper", {
         },
     });
 
+/* ===========ScrollReveal=========== */
 
 const sr = ScrollReveal({
     distance: "50px",
@@ -111,9 +400,11 @@ sr.reveal(`#hero-img, .title, .story-text, .contact_card, .copyright, .footer-so
 
 sr.reveal(`#hero-text-p, #hero-text-h4, #hero-text-h1, #about-img, .text-title, .contact_form, .contact_title`, { origin: "rigth", interval: 100 });
 
-    sr.reveal(`.socil-icon, .hero, .project_card, .btn, .swiper, .projects_filters, .contact_content, .sponsor, .sponsor-img, .links`, { origin: "bottom", interval: 100 });
+    sr.reveal(`.socil-icon, .hero, .project_card, .btn, .swiper, .projects_filters, .contact_content, .sponsor, .sponsor_content, .links`, { origin: "bottom", interval: 100 });
 
 
+
+/* ====================== */
 
 let mixerPortfolio = mixitup('.project_container', {
     selectors: {
